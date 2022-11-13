7168d8928b8140d60efdf66eb32ff3b5e8cd1b19
luhuipeng <973880264@qq.com>
2022-11-13 14:39:41 +0800

    feat: page & component init

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from "./style.module.css";
+
+const Map = () => {
+  return (
+    <>
+      <div>2222</div>
+    </>
+  );
+};
+
+export default Map;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from "./style.module.css";
+import Map from "../../components/Map/Map";
+const Home = () => {
+  return (
+    <>
+      <div>333</div>
+      <Map></Map>
+    </>
+  );
+};
+export default Home;
